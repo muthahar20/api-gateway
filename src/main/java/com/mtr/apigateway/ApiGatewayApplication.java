922697f0aeabc61e5f2f8cb54e4ed17cc0158dd3
muthahar20@gmail.com <muthahar20@gmail.com>
2023-10-31 19:37:51 -0400

    updated with rete limit for incomming request

--- src/main/java/com/mtr/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/mtr/apigateway/ApiGatewayApplication.java
@@ -2,12 +2,23 @@ package com.mtr.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
+	
+	 @Bean
+	    KeyResolver userKeyResolver() {
+	        return exchange -> Mono.just("1");
+	    }
+	
 }
